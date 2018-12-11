4ca7004590f33a0c40cbaeecf9fa75c7389c5daf
Unknown <m_brittle@hotmail.com>
2018-12-11 23:44:48 +0000

    Started work designing the new customer dialog

--- WorkOrganizer/frm_New_Customer.Designer.cs
+++ WorkOrganizer/frm_New_Customer.Designer.cs
@@ -29,22 +29,92 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_fname = new System.Windows.Forms.TextBox();
+            this.txt_sname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_houseNo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(47, 38);
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(57, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Surname";
+            // 
+            // txt_fname
+            // 
+            this.txt_fname.Location = new System.Drawing.Point(75, 19);
+            this.txt_fname.Name = "txt_fname";
+            this.txt_fname.Size = new System.Drawing.Size(121, 20);
+            this.txt_fname.TabIndex = 2;
+            // 
+            // txt_sname
+            // 
+            this.txt_sname.Location = new System.Drawing.Point(283, 19);
+            this.txt_sname.Name = "txt_sname";
+            this.txt_sname.Size = new System.Drawing.Size(100, 20);
+            this.txt_sname.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "House Number";
+            // 
+            // txt_houseNo
+            // 
+            this.txt_houseNo.Location = new System.Drawing.Point(96, 50);
+            this.txt_houseNo.Name = "txt_houseNo";
+            this.txt_houseNo.Size = new System.Drawing.Size(100, 20);
+            this.txt_houseNo.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 83);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Street";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(62, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(202, 21);
+            this.comboBox1.TabIndex = 7;
             // 
             // frm_New_Customer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(590, 419);
+            this.ClientSize = new System.Drawing.Size(633, 451);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt_houseNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt_sname);
+            this.Controls.Add(this.txt_fname);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "frm_New_Customer";
             this.Text = "frm_New_Customer";
@@ -56,5 +126,12 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_fname;
+        private System.Windows.Forms.TextBox txt_sname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_houseNo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
