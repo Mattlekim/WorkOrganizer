83d80357591c3387ad323849898f5274153dc919
Unknown <m_brittle@hotmail.com>
2018-12-15 20:16:54 +0000

    added action state
    added success and 2 failure states

---
+++ WorkOrganizer/Data/ActionState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOrganizer.Data
+{
+    public enum ActionState
+    {
+        Success,
+        Failure_Entry_Alread_Exits,
+        Fail,
+    }
+}
