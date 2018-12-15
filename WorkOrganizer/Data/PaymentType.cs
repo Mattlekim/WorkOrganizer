bc9fa937738ebd40777ddde2bb994c1285da1763
Unknown <m_brittle@hotmail.com>
2018-12-15 20:16:34 +0000

    added payment type

---
+++ WorkOrganizer/Data/PaymentType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOrganizer.Data
+{
+    /// <summary>
+    /// the payment type
+    /// </summary>
+    public enum PaymentType
+    {
+        Cash,
+        Bank,
+        Card,
+        PayPal,
+        Pingit,
+        Check,
+    }
+}
