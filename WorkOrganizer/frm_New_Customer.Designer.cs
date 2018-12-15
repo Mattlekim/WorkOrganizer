4b083e785b17a407deeb29d4b6589347de593d31
Unknown <m_brittle@hotmail.com>
2018-12-15 20:17:19 +0000

    worked on add customer form

--- WorkOrganizer/frm_New_Customer.Designer.cs
+++ WorkOrganizer/frm_New_Customer.Designer.cs
@@ -30,94 +30,214 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.txt_fname = new System.Windows.Forms.TextBox();
-            this.txt_sname = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.txt_houseNo = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
+            this.txt_FName = new System.Windows.Forms.TextBox();
+            this.txt_SName = new System.Windows.Forms.TextBox();
+            this.txt_HouseNo = new System.Windows.Forms.TextBox();
+            this.cb_Street = new System.Windows.Forms.ComboBox();
+            this.bnt_Add = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt_Frequncy = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.bnt_Cancel = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 22);
+            this.label1.Location = new System.Drawing.Point(24, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(57, 13);
+            this.label1.Size = new System.Drawing.Size(48, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "First Name";
+            this.label1.Text = "Forname";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(228, 22);
+            this.label2.Location = new System.Drawing.Point(24, 49);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Surname";
             // 
-            // txt_fname
-            // 
-            this.txt_fname.Location = new System.Drawing.Point(75, 19);
-            this.txt_fname.Name = "txt_fname";
-            this.txt_fname.Size = new System.Drawing.Size(121, 20);
-            this.txt_fname.TabIndex = 2;
-            // 
-            // txt_sname
-            // 
-            this.txt_sname.Location = new System.Drawing.Point(283, 19);
-            this.txt_sname.Name = "txt_sname";
-            this.txt_sname.Size = new System.Drawing.Size(100, 20);
-            this.txt_sname.TabIndex = 3;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Location = new System.Drawing.Point(24, 72);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(78, 13);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "House Number";
-            // 
-            // txt_houseNo
-            // 
-            this.txt_houseNo.Location = new System.Drawing.Point(96, 50);
-            this.txt_houseNo.Name = "txt_houseNo";
-            this.txt_houseNo.Size = new System.Drawing.Size(100, 20);
-            this.txt_houseNo.TabIndex = 5;
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "House No.";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 83);
+            this.label4.Location = new System.Drawing.Point(24, 94);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Street";
+            this.label4.Size = new System.Drawing.Size(66, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Street Name";
+            // 
+            // txt_FName
+            // 
+            this.txt_FName.Location = new System.Drawing.Point(102, 24);
+            this.txt_FName.Name = "txt_FName";
+            this.txt_FName.Size = new System.Drawing.Size(191, 20);
+            this.txt_FName.TabIndex = 4;
+            // 
+            // txt_SName
+            // 
+            this.txt_SName.Location = new System.Drawing.Point(102, 46);
+            this.txt_SName.Name = "txt_SName";
+            this.txt_SName.Size = new System.Drawing.Size(191, 20);
+            this.txt_SName.TabIndex = 5;
+            // 
+            // txt_HouseNo
+            // 
+            this.txt_HouseNo.Location = new System.Drawing.Point(102, 69);
+            this.txt_HouseNo.Name = "txt_HouseNo";
+            this.txt_HouseNo.Size = new System.Drawing.Size(191, 20);
+            this.txt_HouseNo.TabIndex = 6;
+            // 
+            // cb_Street
+            // 
+            this.cb_Street.FormattingEnabled = true;
+            this.cb_Street.Location = new System.Drawing.Point(102, 91);
+            this.cb_Street.Name = "cb_Street";
+            this.cb_Street.Size = new System.Drawing.Size(191, 21);
+            this.cb_Street.TabIndex = 7;
+            // 
+            // bnt_Add
+            // 
+            this.bnt_Add.Location = new System.Drawing.Point(300, 91);
+            this.bnt_Add.Name = "bnt_Add";
+            this.bnt_Add.Size = new System.Drawing.Size(39, 23);
+            this.bnt_Add.TabIndex = 8;
+            this.bnt_Add.Text = "Add";
+            this.bnt_Add.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(24, 117);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Price";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(24, 168);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(94, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Frequncy (Weeks)";
+            // 
+            // txt_Frequncy
+            // 
+            this.txt_Frequncy.Location = new System.Drawing.Point(124, 165);
+            this.txt_Frequncy.Name = "txt_Frequncy";
+            this.txt_Frequncy.Size = new System.Drawing.Size(169, 20);
+            this.txt_Frequncy.TabIndex = 11;
+            this.txt_Frequncy.Text = "4";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(24, 141);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Reqularity";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(62, 80);
+            this.comboBox1.Location = new System.Drawing.Point(102, 138);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(202, 21);
-            this.comboBox1.TabIndex = 7;
+            this.comboBox1.Size = new System.Drawing.Size(191, 21);
+            this.comboBox1.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 114);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(191, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(24, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Notes";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(65, 189);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(228, 107);
+            this.textBox2.TabIndex = 16;
+            // 
+            // bnt_Cancel
+            // 
+            this.bnt_Cancel.Location = new System.Drawing.Point(27, 320);
+            this.bnt_Cancel.Name = "bnt_Cancel";
+            this.bnt_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.bnt_Cancel.TabIndex = 17;
+            this.bnt_Cancel.Text = "Cancel";
+            this.bnt_Cancel.UseVisualStyleBackColor = true;
+            this.bnt_Cancel.Click += new System.EventHandler(this.bnt_Cancel_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(218, 320);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // frm_New_Customer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(633, 451);
+            this.ClientSize = new System.Drawing.Size(346, 355);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.bnt_Cancel);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txt_Frequncy);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.bnt_Add);
+            this.Controls.Add(this.cb_Street);
+            this.Controls.Add(this.txt_HouseNo);
+            this.Controls.Add(this.txt_SName);
+            this.Controls.Add(this.txt_FName);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.txt_houseNo);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txt_sname);
-            this.Controls.Add(this.txt_fname);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "frm_New_Customer";
             this.Text = "frm_New_Customer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frm_New_Customer_FormClosing);
+            this.Shown += new System.EventHandler(this.frm_New_Customer_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,11 +247,22 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txt_fname;
-        private System.Windows.Forms.TextBox txt_sname;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txt_houseNo;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_FName;
+        private System.Windows.Forms.TextBox txt_SName;
+        private System.Windows.Forms.TextBox txt_HouseNo;
+        private System.Windows.Forms.ComboBox cb_Street;
+        private System.Windows.Forms.Button bnt_Add;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt_Frequncy;
+        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button bnt_Cancel;
+        private System.Windows.Forms.Button button1;
     }
 }
--- WorkOrganizer/frm_New_Customer.cs
+++ WorkOrganizer/frm_New_Customer.cs
@@ -8,15 +8,53 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using WorkOrganizer.Data;
+
 namespace WorkOrganizer
 {
     public partial class frm_New_Customer : Form
     {
+        private bool _canClose = true;
+
         public frm_New_Customer()
         {
             InitializeComponent();
         }
 
-      
+        private void bnt_Cancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void frm_New_Customer_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = !_canClose;
+        }
+
+        private void frm_New_Customer_Shown(object sender, EventArgs e)
+        {
+            Data.Street.SetUpDebugData();
+
+            //here we need to build the street list
+            this.cb_Street.Items.Clear();
+            foreach (Street s in Street.Streets)
+                this.cb_Street.Items.Add($"{s.Name}, {s.Area}");
+
+          //  this.cb_Street.Items.Add("Add New Street");
+        }
+
+        //here we save the customer
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Customer customer = new Customer()
+            {
+                FName = txt_FName.Text,
+                SName = txt_SName.Text,
+                HouseName = txt_HouseNo.Text,
+                StreetId = cb_Street.SelectedIndex,
+                // pri
+
+            };
+        }
     }
 }
