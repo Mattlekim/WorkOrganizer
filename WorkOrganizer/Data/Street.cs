ccaf4d3144ebb0e2b96465cbe17fdb3aac10727b
Unknown <m_brittle@hotmail.com>
2018-12-15 20:16:17 +0000

    Added auto incromenter to streets and other stuff

--- WorkOrganizer/Data/Street.cs
+++ WorkOrganizer/Data/Street.cs
@@ -8,14 +8,27 @@ namespace WorkOrganizer.Data
 {
     public class Street
     {
+        /// <summary>
+        /// an autonumber incrementer so that each record has a unique identifyer
+        /// </summary>
+        private static int AutoNumber = 0;
+
+        /// <summary>
+        /// set the id on creation of the instance
+        /// </summary>
+        private readonly int _id = AutoNumber++;
+
+        /// <summary>
+        /// the unique id for this record
+        /// </summary>
+        public int Id { get { return _id; } }
+
         public string Name;
         public string PostCode;
         public string Area;
         public string Notes;
 
-        public int Id { get; private set; }
-
-        public static List<Street> Streets = new List<Street>();
+        public static List<Street> Streets { get; private set; } = new List<Street>();
 
         /// <summary>
         /// Returns the required street
@@ -30,6 +43,33 @@ namespace WorkOrganizer.Data
                     return street; //found the street so return it
 
             return null; //not found so return null
+        }
+
+        public static ActionState Add(Street street)
+        {
+            foreach (Street s in Streets)
+                if (s.Name == street.Name)
+                    if (s.Area == street.Area)
+                    {
+#if DEBUG
+                        Console.WriteLine($"Adding street '{street.Name}, {street.Area}' failed: {ActionState.Failure_Entry_Alread_Exits}");
+#endif
+                        return ActionState.Failure_Entry_Alread_Exits;
+                    }
+            Streets.Add(street);
+#if DEBUG
+            Console.WriteLine($"'{street.Name}, {street.Area}' added.");
+#endif
+            return ActionState.Success;
+        }
+
+        public static void SetUpDebugData()
+        {
+            Console.WriteLine("Creating Debug Streets");
+            Add(new Street() { Name = "test data", Area = "rotherham" });
+            Add(new Street() { Name = "becket cresent", Area = "rotherham" });
+            Add(new Street() { Name = "main street", Area = "rotherham" });
+            Console.WriteLine("Complete");
         }
     }
 }
