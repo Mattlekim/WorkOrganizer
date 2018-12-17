ec834fdb686182f8b8d79afde5cb050cabaede05
Unknown <m_brittle@hotmail.com>
2018-12-17 22:15:34 +0000

    added config file

---
+++ WorkOrganizer/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+using WorkOrganizer.Data;
+
+namespace WorkOrganizer
+{
+    public static class Config
+    {
+        private const string Path = ".\\Config.txt";
+
+        public static void Load()
+        {
+            string[] config = File.ReadAllLines(Path);
+            JobType.SetData(config);
+        }
+    }
+}
