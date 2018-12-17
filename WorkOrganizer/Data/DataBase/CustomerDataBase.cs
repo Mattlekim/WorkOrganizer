9b4868736c0bbb62866188e3958d6deb069bcdb9
Unknown <m_brittle@hotmail.com>
2018-12-17 23:17:56 +0000

    add start of database stuff

---
+++ WorkOrganizer/Data/DataBase/CustomerDataBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOrganizer.Data.DataBase
+{
+    public class CustomerDataBase
+    {
+        public List<Customer> Data = new List<Customer>();
+    }
+}
